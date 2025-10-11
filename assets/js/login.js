14791dd5c35f741a44a996b1d886586aa0cdc6c7
darkalien182 <gunterheronhatsu@gmail.com>
2025-10-11 14:41:56 +0200

    Delete assets directory

--- assets/js/login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-  const username = document.querySelector('input[name="username"]').value.trim();
-  const password = document.querySelector('input[name="password"]').value.trim();
-
-  if (!username || !password) {
-    e.preventDefault();
-    alert('Por favor, completa todos los campos.');
-  }
-});
--- assets/js/profile.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('uploadBtn').addEventListener('click', function() {
-    const form = document.getElementById('uploadForm');
-    form.classList.toggle('hidden');
-  });
-  
